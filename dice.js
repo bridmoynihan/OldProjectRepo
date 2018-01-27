043345d0190b16556c8f6a91b52392bea83082eb
David Donnellan <35601793+davidjdonnellan@users.noreply.github.com>
2018-01-27 12:12:30 +0000

    Add files via upload

--- dice.js
+++ dice.js
@@ -1,69 +1,22 @@
-//Obtaining the players JSON object
-var requestURL = 'https://github.com/phillyb7/3305_project/blob/master/userObject.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-var players;
-request.onload = function() {
-  players = request.response;
-}
+function doubleCount(RandomNumber, RandomNumber2){
+  /* This function compares the dice rolls if 2 of the same number occer
+  on the dice, if a double persists for 3 throws the player goes to jail*/
+var DoubleNumberCount = 0;
 
-//first dice
-var dice = {
-  sides: 6,
-  roll: function () {
-    // + 1 prevents 0 from being rolled
-    var ranNum = Math.floor(Math.random() * this.sides) + 1;
-    return ranNum;
+if(RandomNumber == RandomNumber2){
+  DoubleNumberCount +=1;
+  if DoubleNumberCount == 3{
+    Console.log("Go To Jail Function goes here")
   }
 }
-//second dice 
-var dice2 = {
-  sides: 6,
-  roll: function () {
-    var ranNum2 = Math.floor(Math.random() * this.sides) + 1;
-    return ranNum2;
-  }
+else{
+  DoubleNumberCount = 0;
 }
-//printing the number on the screen
-function printNum(number1) {
-  var dicePosition1 = document.getElementById('dicePosition1');
-  dicePosition1.innerHTML = number1;
-}
-//printing the second number to screen
-function printNum2(number2) {
-  var dicePosition2 = document.getElementById('dicePosition2');
-  dicePosition2.innerHTML = number2;
-}
-//printing the total value on the screen
-function printValue(number1, number2){
-  var Value = document.getElementById('value');
-  var val = number1 + number2;
-  value.innerHTML = val;
-  return val;
 }
 
-function movePlayer(playerID, spacesMoved) {
-  players[playerID]['player_id'] += spacesMoved;
-  /*
-  * We will add all error checking - whether they've gone around the board -
-  * as well as things that may move the player, or if they land on a 
-  * property or pass go later
-  */
+ var json = require('./userObject.json');
+
+function jailTag(){
+  userObj.jail_tag = true;
+  Console.log(userObj);
 }
-
-//define button 
-var button = document.getElementById('button');
-
-//button events on click
-button.onclick = function() {
-  var randNum1 = dice.roll();
-  var randNum2 = dice2.roll();
-  printNum(randNum1);
-  printNum2(randNum2);
-  var movesForward = printValue(randNum1, randNum2);
-  movePlayer(1, movesForwar);
-  //The 1 just above is the placeholder for the player's ID for the time being
-};
-
