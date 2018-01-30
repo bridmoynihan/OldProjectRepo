feb44e2437ffda61e16ea980060c2c6d482fd0fc
seanh521 <35608090+seanh521@users.noreply.github.com>
2018-01-30 16:12:22 +0000

    Add files via upload
    These are JS Objects as they may be easier to access I also have copies of the JSON files so if we decide to use them instead we can

---
+++ userObject.js
@@ -0,0 +1,10 @@
+var userObj= [{
+    player_id:"",
+    capital: 1500,
+    assets:{/*objects placed here*/},
+    jail_card:false,
+    jail_tag:false,
+    colour:{
+      "brown":0, "light-blue":0,"pink":0, "orange":0,"red":0,"green":0, "blue":0}
+    } 
+ ]
